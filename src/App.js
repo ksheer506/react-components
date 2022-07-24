0fba2ffdd78864a67ae3616143058d4f81ade910
ksheer506 <ksheer506@naver.com>
2022-07-24 20:54:22 +0900

    Skeleton 컴포넌트 예시 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import styled from 'styled-components';
 import './App.css';
 import Loading from './components/Loading';
+import Skeleton, { Circle, Rectangle } from './components/Skeleton';
+
+const SkelDiv = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  row-gap: 10px;
+  justify-content: space-between;
+
+  & .text-skel {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    row-gap: 5px;
+  }
+`
 
 function App() {
   return (
     <>
-    <Loading />
+      <Loading />
+
+      <Skeleton width={"300px"} height={"200px"}>
+        <Rectangle width={"300px"} height={"200px"} />
+        <SkelDiv>
+          <Circle radius={"50px"} />
+          <div className='text-skel'>
+            <Rectangle width={"240px"} height={"20px"} />
+            <Rectangle width={"240px"} height={"20px"} />
+          </div>
+        </SkelDiv>
+      </Skeleton>
     </>
   );
 }
